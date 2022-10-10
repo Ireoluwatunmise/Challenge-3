50bad62e4ec1f44a34dbb68ed01f8319337e1ed9
Ireoluwatunmise <111361324+Ireoluwatunmise@users.noreply.github.com>
2022-10-10 12:46:40 +0100

    Solutions to the Bugs in Challenge 3
    1. Making the background move when the game plays instead of when the game is over. (Move left Script)
    
    2. Making the balloon bounce when it touches the ground.
    Also adding a boolean so the balloon can't go higher than the screen
    
    3. Fixed the background to repeat seamlessly
    
    4. Corrected the spelling errors in the Spawn manager script

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public bool gameOver;
+    public bool isLowEnough;
 
     public float floatForce;
     private float gravityModifier = 1.5f;
     private Rigidbody playerRb;
+    private float upperBound = 12f;
 
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
@@ -16,6 +18,7 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip bounceSound;
 
 
     // Start is called before the first frame update
@@ -23,6 +26,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>(); 
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
@@ -32,8 +36,12 @@ public class PlayerControllerX : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.y > upperBound) 
+            isLowEnough = false;
+        else isLowEnough = true;
+        
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
         {
             playerRb.AddForce(Vector3.up * floatForce);
         }
@@ -60,6 +68,11 @@ public class PlayerControllerX : MonoBehaviour
 
         }
 
+        else if (other.gameObject.CompareTag("Ground") && !gameOver) 
+        {
+         playerRb.AddForce(Vector3.up * 11, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound, 1.0f);
+        }
     }
 
 }
